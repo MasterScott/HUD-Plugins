b39b49f39e4c65db4d817f66ee954ffb9ff60f31
coreyblack <coreyblack@gmail.com>
2017-04-22 20:41:11 +0300

    release

---
+++ MarkedForDeathValleyOfDeathPlugin.cs
@@ -0,0 +1,57 @@
+using Turbo.Plugins.Default;
+namespace Turbo.Plugins.CB
+{
+
+    public class MarkedForDeathValleyOfDeathPlugin : BasePlugin, IInGameWorldPainter
+    {
+        public WorldDecoratorCollection MarkedForDeathDecorator { get; set; }
+
+        public MarkedForDeathValleyOfDeathPlugin()
+        {
+            Enabled = true;
+        }
+
+        public override void Load(IController hud)
+        {
+            base.Load(hud);
+
+            
+            
+            MarkedForDeathDecorator = new WorldDecoratorCollection(
+                new GroundCircleDecorator(Hud)
+                {
+                    Brush = Hud.Render.CreateBrush(250, 255, 69, 69, 5),
+                    Radius = 15,
+                },
+                new GroundLabelDecorator(Hud)
+                {
+                    CountDownFrom = 15,
+                    TextFont = Hud.Render.CreateFont("tahoma", 14, 255, 255, 255, 255, true, false, 128, 0, 0, 0, true),
+                },
+                new GroundTimerDecorator(Hud)
+                {
+                    CountDownFrom = 15,
+                    BackgroundBrushEmpty = Hud.Render.CreateBrush(64,0,0,0, 0),
+                    BackgroundBrushFill = Hud.Render.CreateBrush(160,255,255,255, 0),
+                    Radius = 35,
+                }
+            );
+        }
+
+        public void PaintWorld(WorldLayer layer)
+        {
+            var actors = Hud.Game.Actors;
+            foreach (var actor in actors)
+            {
+                switch (actor.SnoActor.Sno)
+                { 
+                   case 230674:
+                        MarkedForDeathDecorator.Paint(layer, actor, actor.FloorCoordinate, null);
+                        break;
+                }
+            }
+        }
+
+    }
+
+} //End
