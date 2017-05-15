536b15476154b7ea4f6f79343750cbafec5c0436
coreyblack <coreyblack@gmail.com>
2017-05-15 22:50:22 +0300

    Added Caltrops Decorator
    Added Caltrops Decorator for all runes

--- MarkedForDeathValleyOfDeathPlugin.cs
+++ MarkedForDeathValleyOfDeathPlugin.cs
@@ -5,6 +5,8 @@ namespace Turbo.Plugins.CB
     public class MarkedForDeathValleyOfDeathPlugin : BasePlugin, IInGameWorldPainter
     {
         public WorldDecoratorCollection MarkedForDeathDecorator { get; set; }
+        public WorldDecoratorCollection CaltropsDecorator { get; set; }
+        
 
         public MarkedForDeathValleyOfDeathPlugin()
         {
@@ -15,7 +17,25 @@ namespace Turbo.Plugins.CB
         {
             base.Load(hud);
 
-            
+            CaltropsDecorator = new WorldDecoratorCollection(
+                new GroundCircleDecorator(Hud)
+                {
+                    Brush = Hud.Render.CreateBrush(250, 255, 69, 69, 5),
+                    Radius = 15,
+                },
+                new GroundLabelDecorator(Hud)
+                {
+                    CountDownFrom = 30,
+                    TextFont = Hud.Render.CreateFont("tahoma", 14, 255, 255, 255, 255, true, false, 128, 0, 0, 0, true),
+                },
+                new GroundTimerDecorator(Hud)
+                {
+                    CountDownFrom = 30,
+                    BackgroundBrushEmpty = Hud.Render.CreateBrush(64,0,0,0, 0),
+                    BackgroundBrushFill = Hud.Render.CreateBrush(160,255,255,255, 0),
+                    Radius = 35,
+                }
+            );
             
             MarkedForDeathDecorator = new WorldDecoratorCollection(
                 new GroundCircleDecorator(Hud)
@@ -47,6 +67,9 @@ namespace Turbo.Plugins.CB
                 { 
                    case 230674:
                         MarkedForDeathDecorator.Paint(layer, actor, actor.FloorCoordinate, null);
+                        break;
+                   case 196030:
+                        CaltropsDecorator.Paint(layer, actor, actor.FloorCoordinate, null);
                         break;
                 }
             }
